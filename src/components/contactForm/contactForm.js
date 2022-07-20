49710b6141ec576c441e6bc34abc3fbf577aba8b
Oleksandr Soloviov <shakal194@gmail.com>
2022-07-20 11:28:41 +0300

    Update contactForm.js

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -57,7 +57,7 @@ function ContactForm({ contacts: { data: contacts } }) {
           <input
             className={s.input}
             type="tel"
-            name="number"
+            name="phone"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
